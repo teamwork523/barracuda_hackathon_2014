41a1da216140259bf37e046485dce47376453f8d
Qi Alfred Chen <adios737@gmail.com>
2014-01-11 04:20:59 -0500

    alfred

--- src/com/barracuda/contest2013/ContestBot.java
+++ src/com/barracuda/contest2013/ContestBot.java
@@ -55,6 +55,7 @@ public class ContestBot {
 
 	// roBAst contest bot
 	public PlayerMessage handleMessage(Message message) {
+		cardsState.debugInfo=false;
 		if (cardsState.debugInfo)
 			System.out.println("----------------------- " + message.type + " --------------------------");
 		if (message.type.equals("request") && game_id != ((MoveMessage)message).state.game_id) {
@@ -76,7 +77,8 @@ public class ContestBot {
 			}
 			
 			// Heurstic Strategy
-			return Strategy.handleMessage(m, cardsState);
+		//	return Strategy.handleMessage(m, cardsState);
+			return (new BruteForceSearch()).handleMessage(m, cardsState);
 		}
 		else if (message.type.equals("result")) {
 			ResultMessage r = (ResultMessage)message;
