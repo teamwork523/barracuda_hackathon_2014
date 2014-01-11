3da853556e7b39729cf04c65f8c1149a23efc083
Snooperlock <teamworkucla@hotmail.com>
2014-01-10 23:28:10 -0500

    update strategy

--- src/com/barracuda/contest2013/Strategy.java
+++ src/com/barracuda/contest2013/Strategy.java
@@ -4,9 +4,26 @@ public class Strategy {
   /******************************************************/
   /******************* Hand Strategy ********************/
   /******************************************************/
+  public static int findTheSmallestCard(int[] hands) {
+    int minCard = 14;
+    for (int i = 0; i < hands.length; i++) {
+      if (hands[i] < minCard) {
+        minCard = hands[i];
+      }
+    }
+    return minCard;
+  }
   
+  // TODO: 
   /******************************************************/
   /************** Challenge Strategy ********************/
   /******************************************************/
   
+  public static boolean majorityGreaterThanThreshold(int[] hands, 
+                                                     GameState state, 
+                                                     Cards cards,
+                                                     double threshold) {
+    int 
+    return true;
+  }
 }
