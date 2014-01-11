82fd9dcd21cc1b29e0181ba28efc87077acd58e8
Qi Alfred Chen <adios737@gmail.com>
2014-01-10 22:30:44 -0500

    bfs

---
+++ src/com/barracuda/contest2013/BruteForceSearch.java
@@ -0,0 +1,12 @@
+package com.barracuda.contest2013;
+
+import java.util.ArrayList;
+
+public class BruteForceSearch {
+	
+	void getResult(MoveMessage mm, Cards cc){
+		ArrayList<Integer> oph;
+		
+	}
+
+}
